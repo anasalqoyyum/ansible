f969c40e54ab11a2d3ee6939b9a7e8580af564f6
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2025-03-29 20:49:14 +0700

    chore: backing up the missing pwsh stuff

---
+++ conf/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+# In a way, I don't know how to properly document this file. It's a PowerShell profile file that I use to configure my PowerShell environment. 
+# It's a mix of things that I've found useful over time. 
+# Other things installed: mise, uv, starship, PowerToys CommandNotFound module, Chocolatey, scoop
+
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\starship.toml"
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+mise activate pwsh | Out-String | Invoke-Expression
