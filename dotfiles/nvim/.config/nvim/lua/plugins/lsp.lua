300bf1b571e67f626e96a65d91510a6588ca7eaa
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2025-07-17 18:38:33 +0700

    feat(vim): add luasnip and replicate vscode cmp in blink

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -35,6 +35,14 @@ return {
         ghost_text = {
           enabled = vim.g.ai_cmp,
         },
+        list = {
+          selection = {
+            preselect = function(ctx)
+              return not require("blink.cmp").snippet_active({ direction = 1 })
+            end,
+            auto_insert = true,
+          },
+        },
       },
 
       -- experimental signature help support
@@ -52,10 +60,38 @@ return {
       },
 
       keymap = {
-        preset = "enter",
+        preset = "none",
+
+        -- this is basically super-tab and enter to accept similar to vscode
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+
         -- manually set some due to it's not being picked up on macos lol
         ["<C-y>"] = { "select_and_accept" },
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       },
     },
   },
